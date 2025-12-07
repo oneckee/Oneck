7a727a8b0e4411a27ec62a1d64fe448450611fd4
Oneck <oneck@Oneckui-MacBookAir.local>
2025-12-07 13:22:23 +0900

    Move .py files to IMU_Test folder

--- flight_control_computer.py
+++
@@ -1,6 +0,0 @@
-class FlightControlComputer:
-    def compute_control(self, roll, pitch):
-        # 단순한 제어법칙 예시
-        aileron = -roll * 10    # Roll 안정화
-        elevator = -pitch * 8   # Pitch 안정화
-        return aileron, elevator
